7183ae962e3f69c697bc3bdd0d32d0219267da4c
Field-Effect-Transistor <145497915+Field-Effect-Transistor@users.noreply.github.com>
2025-11-26 23:47:20 +0200

    Delete .pio/build directory

--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- /home/Vincente/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/Vincente/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- .pio/build/esp32dev/lib7b5/QPESP32/qstamp.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/lib7b5/QPESP32/qstamp.cpp.o: \
- .pio/libdeps/esp32dev/QPESP32/src/qstamp.cpp \
- .pio/libdeps/esp32dev/QPESP32/src/qstamp.hpp
